fa8bd4e42f496c2010a01de2e2716d04e752131f
kurepa <cristian3629@gmail.com>
2016-09-09 15:53:05 -0300

    agregando vector y getters de Matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,4 +1,23 @@
-public class Matrix(){
+class Matrix(){
+  int cantColumns, cantRows;
+public:
+   Matrix(int colum, int rows){
+     //tengo que ver como creo la matrix
+  }
+
+  //getters
+  int getCantColum(){
+    return cantColum;
+  }
+
+  int getCantRows(){
+    return this.cantRows;
+  }
+
+
+  int getElementPos(int posColumn, int posRows){
+      //
+  }
 
 
 
